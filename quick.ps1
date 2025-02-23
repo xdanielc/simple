30cbc84f5bb1795b0840c0bd1fbfb94fa2ce8714
xdanielc <xdanielcalderon@gmail.com>
2025-02-24 00:13:55 +0100

    quick commit

--- quick.ps1
+++ quick.ps1
@@ -1,4 +1,7 @@
 Get-ChildItem *.php | ForEach-Object { php $_ > "./dist/$($_.BaseName).html" }
+Function Show-MsgBox ($Text,$Title="",[Windows.Forms.MessageBoxButtons]$Button = "OK",[Windows.Forms.MessageBoxIcon]$Icon="Information"){
+    [Windows.Forms.MessageBox]::Show("$Text", "$Title", [Windows.Forms.MessageBoxButtons]::$Button, $Icon) | ?{(!($_ -eq "OK"))}
+    }
 git add .
 git commit -m "quick commit"
 git push
